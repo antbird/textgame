f403a51ebb7bebabcea234199b3fafbbfb707fac
Michael McIntyre <antbird@caffeinatednoise.com>
2014-06-07 12:27:05 -0500

    IMPORTANT: DISABLES FOREIGN KEY CHECKS. Also, add references to other seeders

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -11,7 +11,12 @@ class DatabaseSeeder extends Seeder {
 	{
 		Eloquent::unguard();
 
-		// $this->call('UserTableSeeder');
+		DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+		
+		$this->call('ZonesTableSeeder');
+		$this->call('UsersTableSeeder');
+		$this->call('ActionsTableSeeder');
+		$this->call('EventsTableSeeder');
 	}
 
 }
