0f478404336be32529e1f985607c9c50afbd6b6e
Michael McIntyre <antbird@caffeinatednoise.com>
2014-06-07 12:25:24 -0500

    Add resource routes; begin structure of main route

--- app/routes.php
+++ app/routes.php
@@ -15,3 +15,26 @@ Route::get('/', function()
 {
 	return View::make('hello');
 });
+
+Route::resource('zones', 'ZonesController');
+Route::resource('actions', 'ActionsController');
+Route::resource('events', 'EventsController');
+
+Route::get('action/{id}', function($id)
+{
+	//check if valid action
+	$action = Action::find($id);
+	if (!$action) {
+		return "Invalid action.";
+	}
+
+	return $action;
+
+	//go through each event based on priority and matching conditions
+
+	//execute triggers for matching event
+
+	/* print out current zone name & description
+	(basic, later we'll push json data to update each affected gui panel) */
+	
+});
